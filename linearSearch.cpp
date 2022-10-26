a95cccf446dd1e6bbd03e12c829bbc2056d21322
Aaditya dubey <115134350+Aaditya14x@users.noreply.github.com>
2022-10-26 14:24:51 +0530

    Add files via upload

---
+++ linearSearch.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+bool search(int arr[],int size, int key){
+
+}
+int main(){
+int arr[10] = {10,456,1000,25,43,32,617,999,25,90}:
+cout<<"Enter the element to search for "<<endl;
+int key:
+cin >> key:
+bool found = search(arr , 10 , key);
+if(Found){
+    cout<<"Key is present"<<endl;
+}
+else{
+    cout<<"key is absent"<<endl;
+}
+return 0;
+}
